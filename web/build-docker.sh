8617a1f35bbddf1d4c4fff2e74fe5acb86faeb8e
alphabetapeter <peter.moesenthin@gmail.com>
2018-02-05 22:33:22 +0100

    fix docker build command for web

--- web/build-docker.sh
+++ web/build-docker.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-docker rm -f spotify-playlist-manager && docker rmi -f spotify-playlist-manager
-docker build . -t spotify-playlist-manager
-docker run -d --name=spotify-playlist-manager -p 8089:80 spotify-playlist-manager
+docker build . -t philips-hue-spotify-connector-web:latest
